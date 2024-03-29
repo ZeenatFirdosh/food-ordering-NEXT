33da275b31a7c6b3a101742eeaebdedaa3fb4717
Zeenat <zeenatzfq052@gmail.com>
2024-03-29 11:17:42 +0530

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,23 @@
-import {AppProvider} from "@/components/AppContext";
+import { AppProvider } from "@/components/AppContext";
 import Header from "@/components/layout/Header";
-import { Roboto } from 'next/font/google'
-import './globals.css'
-import './style.css'
-import {Toaster} from "react-hot-toast";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+import "./style.css";
+import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
-const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] })
+const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
 export const metadata = {
-  title: 'Food Ordering App',
-  description: 'Food Ordering App',
-}
+  title: "Food Ordering App",
+  description: "Food Ordering App",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body className={roboto.className}>
+       
         <main className="max-w-4xl mx-auto p-4">
           <AppProvider>
             <Toaster />
@@ -28,5 +30,5 @@ export default function RootLayout({ children }) {
         </main>
       </body>
     </html>
-  )
+  );
 }
