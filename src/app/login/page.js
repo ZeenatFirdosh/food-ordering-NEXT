2d0aef861492d826d052f059bd843ef9e4bedca8
Zeenat <zeenatzfq052@gmail.com>
2024-03-29 10:25:33 +0530

    apostrophe error

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,18 +1,18 @@
-'use client';
-import {signIn} from "next-auth/react";
+"use client";
+import { signIn } from "next-auth/react";
 import Image from "next/image";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [loginInProgress, setLoginInProgress] = useState(false);
 
   async function handleFormSubmit(ev) {
     ev.preventDefault();
     setLoginInProgress(true);
 
-    await signIn('credentials', {email, password, callbackUrl: '/'});
+    await signIn("credentials", { email, password, callbackUrl: "/" });
 
     setLoginInProgress(false);
   }
@@ -21,23 +21,29 @@ export default function LoginPage() {
       <h3 className="text-center text-primary font-extrabold text-4xl mb-4">
         Login
       </h3>
-      <form className="max-w-xs mx-auto" onSubmit={handleFormSubmit}>
-        {/* <input type="email" name="email" placeholder="email" value={email}
+      {/* <form className="max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+        <input type="email" name="email" placeholder="email" value={email}
                disabled={loginInProgress}
                onChange={ev => setEmail(ev.target.value)} />
         <input type="password" name="password" placeholder="password" value={password}
                disabled={loginInProgress}
                onChange={ev => setPassword(ev.target.value)}/>
-        <button disabled={loginInProgress} type="submit">Login</button> */}
+        <button disabled={loginInProgress} type="submit">Login</button>
+      </form> */}
+
+      <div className="max-w-xs mx-auto">
         <div className="my-4 text-center text-gray-500">
           login with provider
         </div>
-        <button type="button" onClick={() => signIn('google', {callbackUrl: '/'})}
-                className="flex gap-4 justify-center">
-          <Image src={'/google.png'} alt={''} width={24} height={24} />
+        <button
+          type="button"
+          onClick={() => signIn("google", { callbackUrl: "/" })}
+          className="flex gap-4 justify-center"
+        >
+          <Image src={"/google.png"} alt={""} width={24} height={24} />
           Login with google
         </button>
-      </form>
+      </div>
     </section>
   );
 }
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,12 +1,12 @@
 "use client";
-import {signIn} from "next-auth/react";
+import { signIn } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function RegisterPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [creatingUser, setCreatingUser] = useState(false);
   const [userCreated, setUserCreated] = useState(false);
   const [error, setError] = useState(false);
@@ -15,15 +15,14 @@ export default function RegisterPage() {
     setCreatingUser(true);
     setError(false);
     setUserCreated(false);
-    const response = await fetch('/api/register', {
-      method: 'POST',
-      body: JSON.stringify({email, password}),
-      headers: {'Content-Type': 'application/json'},
+    const response = await fetch("/api/register", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
     });
     if (response.ok) {
       setUserCreated(true);
-    }
-    else {
+    } else {
       setError(true);
     }
     setCreatingUser(false);
@@ -35,19 +34,23 @@ export default function RegisterPage() {
       </h3>
       {userCreated && (
         <div className="my-4 text-center">
-          User created.<br />
-          Now you can{' '}
-          <Link className="underline" href={'/login'}>Login &raquo;</Link>
+          User created.
+          <br />
+          Now you can{" "}
+          <Link className="underline" href={"/login"}>
+            Login &raquo;
+          </Link>
         </div>
       )}
       {error && (
         <div className="my-4 text-center">
-          An error has occurred.<br />
+          An error has occurred.
+          <br />
           Please try again later
         </div>
       )}
-      <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
-        {/* <input type="email" placeholder="email" value={email}
+      {/* <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+        <input type="email" placeholder="email" value={email}
                disabled={creatingUser}
                onChange={ev => setEmail(ev.target.value)} />
         <input type="password" placeholder="password" value={password}
@@ -55,21 +58,26 @@ export default function RegisterPage() {
                 onChange={ev => setPassword(ev.target.value)}/>
         <button type="submit" disabled={creatingUser}>
           Register
-        </button> */}
+        </button>
+      </form> */}
+      <div className="max-w-xs mx-auto">
         <div className="my-4 text-center text-gray-500">
-           login with provider
+          login with provider
         </div>
         <button
-          onClick={() => signIn('google', {callbackUrl:'/'})}
-          className="flex gap-4 justify-center">
-          <Image src={'/google.png'} alt={''} width={24} height={24} />
+          onClick={() => signIn("google", { callbackUrl: "/" })}
+          className="flex gap-4 justify-center"
+        >
+          <Image src={"/google.png"} alt={""} width={24} height={24} />
           Login with google
         </button>
         <div className="text-center my-4 text-gray-500 border-t pt-4">
-          Existing account?{' '}
-          <Link className="underline" href={'/login'}>Login here &raquo;</Link>
+          Existing account?{" "}
+          <Link className="underline" href={"/login"}>
+            Login here &raquo;
+          </Link>
         </div>
-      </form>
+      </div>
     </section>
   );
 }
